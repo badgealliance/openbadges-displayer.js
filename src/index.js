d7433cbb541c36e9c7fb0bf63f4c30bca1fa0633
Joe Curlee <joe.curlee@gmail.com>
2014-06-30 14:53:07 -0500

    made plugin slightly easier to extend. updated readme.

--- src/index.js
+++ src/index.js
@@ -5,75 +5,80 @@ var insertCss = require('insert-css');
 var css = fs.readFileSync(__dirname + '/badgeFlip.css');
 var $ = require('jquery');
 
-var _parser = function(images, callback) {
-  var badge = {};
-  var xhr = null;
-  var baked = null;
-  var json = null;
+function MozBadgeParser() {
+  this.selector = 'img.moz-open-badge';
 
-  _.each(images, function(i) {
-    console.log("loading ", i.src);
+  this.parse = function() {
+    var badge = {};
+    var xhr = null;
+    var baked = null;
+    var json = null;
 
-    badge = {}
-    xhr = new XMLHttpRequest();
-    
-    xhr.open('GET', i.src, true);
-    xhr.responseType = 'arraybuffer';
-    xhr.onload = function(e) {
-      if (this.status == 200) {
-        baked = baker(this.response);
+    _.each(self.elems, function(i) {
+      console.log("loading ", i.src);
 
-        // Strip non-ascii characters.
-        // Using regex found here: http://stackoverflow.com/a/20856252
-        json = baked.textChunks['openbadges'].replace(
-          /[^A-Za-z 0-9 \.,\?""!@#\$%\^&\*\(\)-_=\+;:<>\/\\\|\}\{\[\]`~]*/g,
-          ''
-        );
-        
-        badge.assertion = JSON.parse(json);
+      badge = {}
+      xhr = new XMLHttpRequest();
+      
+      xhr.open('GET', i.src, true);
+      xhr.responseType = 'arraybuffer';
+      xhr.onload = function(e) {
+        if (this.status == 200) {
+          baked = baker(this.response);
 
-        badge.image = i.src;
-        badge.el = i;
-        if (callback) {
-          callback(badge);
+          // Strip non-ascii characters.
+          // Using regex found here: http://stackoverflow.com/a/20856252
+          json = baked.textChunks['openbadges'].replace(
+            /[^A-Za-z 0-9 \.,\?""!@#\$%\^&\*\(\)-_=\+;:<>\/\\\|\}\{\[\]`~]*/g,
+            ''
+          );
+          
+          badge.assertion = JSON.parse(json);
+
+          badge.image = i.src;
+          badge.el = i;
+          if (self.cb) {
+            self.cb(badge);
+          }
         }
       }
-    }
 
-    xhr.send();
-  });
-
+      xhr.send();
+    });
+  }
 }
 
 var ParseBadges = function() {
-  var cb = null;
-  var imgs = [];
-  var args = arguments.length;
+  var self = this;
+  self.parser = new MozBadgeParser;
 
-  if (args < 0 || args > 2) {
+  self.cb = null;
+  self.elems = [];
+  self.args = arguments.length;
+
+  if (self.args < 0 || self.args > 2) {
     throw("usage: ParseBadges(images, callback) or ParseBadges() if used as a browser script");
     return;
   }
 
   if (args == 2) {
-    imgs = arguments[0];
-    cb = arguments[1];
+    self.elems = arguments[0];
+    self.cb = arguments[1];
   }
   else {
-    imgs = $(document).find('img');
+    self.elems = $(document).find(self.parser.selector);
 
     if (args == 1) {
-      cb = arguments[0];
+      self.cb = arguments[0];
     }
   }
 
-  _parser(imgs, cb);
+  self.parser.parse();
 }
 
 module.exports.ParseBadges = ParseBadges;
 $(document).ready(
   function() {
     window.ParseBadges = ParseBadges;
-    //ParseBadges();
   }
 );
